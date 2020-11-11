264a91ddef833eedbcdba4053281d6336bac0b96
wanni0928 <chlgudwnd123@gmail.com>
2020-11-11 12:41:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+import InputSample from './InputSample';
+import './App.css'
+import UserList from './UserList';
 
-function App() {
+const App = () => {
+
+  const name = 'react';
+  const style = {
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:24,
+    padding: '1rem'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Wrapper>
+        {/*주석은 화면에 안보인다.*/}
+        /*중괄호로 감싸지 않으면 클라이언트단에 출력된다.*/
+        <Hello name="react" color="red" isSpecial={true}
+          // 열리는 태그 내부에서는 일허게 주석을 작성 할 수 있다.
+        />
+        <Hello />
+        <Hello />
+        <div style={style}>{name}</div>
+        <div className='gray-box'></div>
+        <Counter />
+        <InputSample />
+        <UserList />
+      </Wrapper>
   );
 }
 

ff33aeadbd26242906fc48b7258265666168aa1c
wanni0928 <chlgudwnd123@gmail.com>
2020-11-11 20:40:29 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './Counter';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  // <Counter />
+  ,
   document.getElementById('root')
 );
 
